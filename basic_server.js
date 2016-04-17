60b463c8e68dc3d804567ca7eec57a0fd2d0634f
shabaz123 <shabaz123-github@outlook.com>
2016-04-17 01:18:24 +0100

    Basic web server
    Basic web server (serves up the results of a ping command)

---
+++ basic_server.js
@@ -0,0 +1,27 @@
+#!/usr/bin/nodejs
+
+console.log("Hello");
+var app = require('http').createServer(handler);
+var child = require('child_process');
+app.listen(8090);
+
+
+//HTML handler
+function handler (req, res)
+{
+sel=req.url.substr(1);
+remote_ip=req.connection.remoteAddress;
+console.log("Got a request from "+remote_ip);
+if (sel != "diskstat.html")
+{
+  sel = "diskstat.html"; // only allow this file for now
+}
+
+res.writeHead(200);
+prog=child.exec("ping -c 4 "+remote_ip, function(error, data, stderr)
+{
+  res.end(data);
+});
+
+} // end of HTML handler
+
